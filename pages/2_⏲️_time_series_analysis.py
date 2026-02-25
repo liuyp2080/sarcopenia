f7f149877b7a9cf401f0c8bf5d2d8c5d7ca80197
liuyp2080 <78184640+liuyp2080@users.noreply.github.com>
2026-02-25 10:24:16 +0800

    Update 2_⏲️_time_series_analysis.py

--- pages/2_⏲️_time_series_analysis.py
+++ pages/2_⏲️_time_series_analysis.py
@@ -24,7 +24,7 @@ def check_password():
         """Form with widgets to collect user information"""
         with st.form("Credentials"):
             st.text_input("Username", key="username")
-            st.text_input("Password", type="password", key="password")
+            st.text_input("Password", type="passwords", key="passwords")
             st.form_submit_button("Log in", on_click=password_entered)
 
     def password_entered():
@@ -32,11 +32,11 @@ def check_password():
         if st.session_state["username"] in st.secrets[
             "passwords"
         ] and hmac.compare_digest(
-            st.session_state["password"],
+            st.session_state["passwords"],
             st.secrets.passwords[st.session_state["username"]],
         ):
             st.session_state["password_correct"] = True
-            del st.session_state["password"]  # Don't store the username or password.
+            del st.session_state["passwords"]  # Don't store the username or password.
             del st.session_state["username"]
         else:
             st.session_state["password_correct"] = False
@@ -250,3 +250,4 @@ with st.container():
         mime='text/csv',
     )
     st.dataframe(df_results,use_container_width=True)
+
